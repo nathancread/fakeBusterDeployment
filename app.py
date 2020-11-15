a0a575c1fdabf52057976626013b09ede3241076
nathancread <nathancread@gmail.com>
2020-11-15 15:34:13 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, Response, jsonify
+from flask_cors import CORS
+from datetime import datetime
+import os
+
+##TODO
+    #firebase setup
+    #heroku setup
+
+# Initialize the Flask application
+app = Flask(__name__)
+CORS(app)
+
+
+
+@app.route('/test', methods=['GET'])
+def test():
+    jsonify({"success": True}), 200
+
+#start flask app
+if __name__ == '__main__':
+    app.run()
+#host="localhost", port=5000
