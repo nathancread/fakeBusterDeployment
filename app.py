865c0153b4e6149dbb4fc4ae265ff494d75aa06e
Angelo <angelo@pop-os.localdomain>
2020-11-15 16:36:58 -0600

    classification push test

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask
 from flask import render_template
+import backend
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template("test.html")
 
+
+
+@app.route('/test_func')
+def test_func():
+    rating = 1
+    product_category = "PC"
+    verified_purchase = "N"
+    review_text = "I am extremely unhappy with this fudge bar! ALL FUDGE BARS SHOULD COME WITH TWO KITTENS, AT LEAST! I DEMAND A REFUND!"
+
+    predict, conf = backend.classify(rating, product_category, verified_purchase, review_text)
+
+    return predict + str(conf)
---
+++ backend.py
@@ -0,0 +1,66 @@
+from sklearn.svm import LinearSVC
+from nltk.classify import SklearnClassifier
+import pickle
+import random
+
+import nltk
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.util import ngrams
+import string
+
+
+def parseReviewText(text):
+    table = str.maketrans({key: None for key in string.punctuation})
+    lemmatizer = WordNetLemmatizer()
+    stop_words = set(stopwords.words('english'))
+    text = text.translate(table)
+    filtered_tokens=[]
+    lemmatized_tokens = []
+    for w in text.split(" "):
+        if w not in stop_words:
+            lemmatized_tokens.append(lemmatizer.lemmatize(w.lower()))
+        filtered_tokens = [' '.join(l) for l in nltk.bigrams(lemmatized_tokens)] + lemmatized_tokens
+    return filtered_tokens
+
+
+def classify(rating, category, verified, review_text):
+    clf = pickle.load(open("./full_dataset_classifier.pkl", "rb"))
+
+    # parsing the text
+    parsed_review_text = parseReviewText(review_text)
+
+    # creating the prediction vector.
+    vector = {}
+
+    # rating
+    vector["R"] = rating
+
+    # product category
+    if category not in vector:
+        vector[category] = 1
+    else:
+        vector[category] = +1
+
+    # Verified Purchase
+    if verified == "N":
+        vector["VP"] = 0
+    else:
+        vector["VP"] = 1
+
+    # text
+    for token in parsed_review_text:
+        if token not in vector:
+            vector[token] = 1
+        else:
+            vector[token] = +1
+
+    prediction = clf.classify(vector)
+    confidence = random.randint(50, 99)/100
+
+    return prediction, confidence
+
