957a4867a41e74149ad8e4a874c0c844ff0141af
Angelo <angelo@pop-os.localdomain>
2020-11-15 17:01:26 -0600

    add print statements

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 import backend
+import sys
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,7 +16,8 @@ def test_func():
     product_category = "PC"
     verified_purchase = "N"
     review_text = "I am extremely unhappy with this fudge bar! ALL FUDGE BARS SHOULD COME WITH TWO KITTENS, AT LEAST! I DEMAND A REFUND!"
-
+    print("NICE")
     predict, conf = backend.classify(rating, product_category, verified_purchase, review_text)
-
+    print("NICER2")
+    sys.stdout.flush()
     return predict + str(conf)
