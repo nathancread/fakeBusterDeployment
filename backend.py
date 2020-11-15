6c4eed8422d50d1eed8ef83f152128123d02f143
Angelo <angelo@pop-os.localdomain>
2020-11-15 16:54:45 -0600

    remove download needs

--- backend.py
+++ backend.py
@@ -6,8 +6,8 @@ import random
 import nltk
 from nltk.tokenize import word_tokenize
 # cant access /usr/ on heroku so we download locally.
-nltk.download('punkt')
-nltk.download('wordnet')
+# nltk.download('punkt')
+# nltk.download('wordnet')
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
