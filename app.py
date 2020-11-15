34c083299bdac41f11717f3486a5f0a4261a6632
nathancread <nathancread@gmail.com>
2020-11-15 15:52:10 -0600

    simple app

--- app.py
+++ app.py
@@ -1,23 +1,6 @@
-from flask import Flask, request, Response, jsonify
-from flask_cors import CORS
-from datetime import datetime
-import os
-
-##TODO
-    #firebase setup
-    #heroku setup
-
-# Initialize the Flask application
+from flask import Flask
 app = Flask(__name__)
-CORS(app)
 
-
-
-@app.route('/test', methods=['GET'])
-def test():
-    return jsonify({"success": True}), 200
-
-#start flask app
-if __name__ == '__main__':
-    app.run()
-#host="localhost", port=5000
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
