2f4066a352bd97298e2049c317f5c4f813447320
Angelo Yang <angelo.r.yang@gmail.com>
2020-11-16 01:55:03 -0600

    whoops ;) wasn't me i swear

--- app.py
+++ app.py
@@ -53,21 +53,5 @@ def index():
 
 
 
-
-
-@app.route('/test_func')
-def test_func():
-    reviews, title, image_url = backend.scrape("https://www.amazon.com/Enhanced-Splashproof-Portable-Bluetooth-Radiator/dp/B010OYASRG/ref=sr_1_3?dchild=1&keywords=bluetooth%2Bspeaker&qid=1605484686&sr=8-3&th=1")
-
-    test_string = title + "\n"
-
-    for review in reviews:
-        predict, conf = backend.classify(review["rating"], review["category"], review["verified"], review["review_text"])
-        test_string += predict + " " + str(conf) +"\n"
-
-    sys.stdout.flush()
-    return test_string
-
-
 if __name__ == '__main__':
     app.run(debug=True)
