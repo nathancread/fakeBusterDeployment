e441c21afe000f6411166e493bb8ce013f93bea3
nathancread <nathancread@gmail.com>
2020-11-15 19:00:26 -0600

    calculate all the stats

--- app.py
+++ app.py
@@ -8,14 +8,32 @@ app = Flask(__name__, template_folder='templates')
 def hello_world():
     return render_template("search.html")
 
+#title,image,percentage_FAKE_reviews,stars_without_fake,stars_with_fake
 @app.route('/reviews/<url>')
 def reviews(url):
     reviews, title, image_url  = backend.scrape(url)
     test_string = title+ "\n"+image_url+"\n"
+    fake = 0
+    total = 0
+    total_real = 0 
+    total_stars_real = 0
+    total_stars = 0
+
     for review in reviews:
         predict, conf = backend.classify(review["rating"], review["category"], review["verified"], review["review_text"])
-        test_string += predict + " " + str(conf) +"\n"
-        return test_string
+        total+=1
+        total_stars += review["rating"]
+        if predict == "FAKE":
+            fake +=1
+        if predict == "REAL":
+            total_real+=1
+            total_stars_real += review["rating"]
+
+    test_string += fake/total +"\n"
+    test_string += total_stars_real/total_real + "\n"
+    test_string += total_stars/total +"\n"
+
+    #return test_string
 
 
 @app.route('/test_func')
